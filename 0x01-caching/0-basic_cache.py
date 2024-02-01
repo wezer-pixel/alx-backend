129f0802ebbddf33b645de22f3632f81991d651d
wezer-pixel <jerryjuma104@gmail.com>
2024-02-01 12:23:41 +0300

    [TASK]Basic Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,13 +2,13 @@
 
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
 	def put(self, key, item):
 		if key and item:
 			self.cache_data[key] = item
 		else:
 			return None
-
 
 	def get(self, key):
 		if key in self.cache_data:
